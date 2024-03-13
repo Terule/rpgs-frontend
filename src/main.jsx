4e8f3805ad34c0efc33bcf11a04c5ea4f3496d2f
Rafael Aguiar <terule@gmail.com>
2024-03-13 16:21:57 -0300

    feat: Add dark theme and SnackbarProvider to main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { SnackbarProvider } from 'notistack'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { appRouter } from './utils/router'
+import { dark } from './themes/dark'
+import { router } from './utils/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider  router={appRouter} />
+    <ThemeProvider theme={dark}>
+      <SnackbarProvider>
+        <CssBaseline />
+        <RouterProvider  router={router} />
+      </SnackbarProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
