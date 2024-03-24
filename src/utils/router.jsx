1a3911fa3f3107a71b1b7a6c46746b57d9c978e4
Rafael Aguiar <terule@gmail.com>
2024-03-24 16:06:00 -0300

    feat: Add new pages and routes for login and register

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,15 +1,18 @@
 
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import Error404 from "../pages/Error404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <HomePage />,
+    errorElement: <Error404 />,
+    children: [
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+    ],
   },
-  {
-    path: "/home",
-    element: <HomePage />
-  }
 ]);
