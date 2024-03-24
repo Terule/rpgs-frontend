c4377e8fced1530823779d687c37d81b214f1f40
Rafael Aguiar <terule@gmail.com>
2024-03-24 16:06:21 -0300

    style: Add font weights for Roboto to main component

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { dark } from './themes/dark'
 import { router } from './utils/router'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
